60088837140605893ba6fd815ae3e28ca8bb2fcc
Enzo Maruffa <enzomm1999@gmail.com>
2019-04-24 13:37:29 -0300

    Creates the view that is shared between other views. Also, instructions.

---
+++ Colorboard/ColorsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorsViewController.swift
+//  Colorboard
+//
+//  Created by Enzo Maruffa Moreira on 24/04/19.
+//  Copyright © 2019 Enzo Maruffa Moreira. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
